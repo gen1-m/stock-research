f82aad26fd4002316393454221e53bd28b3253b7
UncrownedKing1 <108270526+UncrownedKing1@users.noreply.github.com>
2023-10-18 10:25:27 +0200

    Configured a new font and configured the image domains.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    fontFamily: {
+      'pint': [
+        "Mona Sans", 
+        "Mona Sans Header Fallback", 
+        "-apple-system", 
+        "BlinkMacSystemFont", 
+        "Segoe UI", 
+        "Helvetica", 
+        "Arial", 
+        "sans-serif", 
+        "Apple Color Emoji", 
+        "Segoe UI Emoji", 
+        "Segoe UI Symbol"
+      ]
+    }
+  },
   plugins: [],
 }
